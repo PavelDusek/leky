bb80c640f5c883dfb0762ce7f62bc4c4f99459d6
Pavel Dusek <pavel.dusek@gmail.com>
2020-07-05 23:30:12 +0200

    extracting data using pdftotext and regular expressions

--- analyze_spc.py
+++ analyze_spc.py
@@ -3,8 +3,45 @@ import pdftotext
 import pandas as pd
 import re
 
+getName = re.compile("název přípravku(.+)kvalitativní a kvantitativní složení", re.IGNORECASE | re.DOTALL | re.UNICODE )
+getForm = re.compile("léková forma(.+)klinické údaje", re.IGNORECASE | re.DOTALL | re.UNICODE )
+garbage = re.compile(" {2,}|2\.$|4\.$")
+
+names, forms = [], []
+
+def extractData( pdf ):
+    try:
+        text = pdf[0] #first page
+        name = getName.findall(text)[0]
+        form = getForm.findall(text)[0]
+        name = name.strip()
+        form = form.strip()
+        name = garbage.sub("", name.strip())
+        form = garbage.sub("", form.strip())
+        name = name.strip()
+        form = form.strip()
+        return name, form
+    except Exception:
+        text = pdf[1] #second page
+        name = getName.findall(text)[0]
+        form = getForm.findall(text)[0]
+        name = name.strip()
+        form = form.strip()
+        name = garbage.sub("", name.strip())
+        form = garbage.sub("", form.strip())
+        name = name.strip()
+        form = form.strip()
+        return name, form
+
 for file in glob.glob("spc/*.pdf"):
-    with open(file, "rb") as f:
-        pdf = pdftotext.PDF(f)
-    text = pdf[0] #first page
-    print(text)
+    try:
+        with open(file, "rb") as f:
+            pdf = pdftotext.PDF(f)
+        name, form = extractData(pdf)
+        names.append(name)
+        forms.append(form)
+    except Exception: pass
+
+df = pd.DataFrame( {'names': names, 'forms': forms} )
+df.to_csv('forms.csv')
+df.to_excel('forms.xlsx')
